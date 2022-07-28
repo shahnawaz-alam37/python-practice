588913256e56030e44ca5185a9fbecb25e689a36
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-07-28 22:14:30 +0530

    data type checking

---
+++ day1/helloworld.py
@@ -0,0 +1,8 @@
+print(type(10))                       #int
+print(type(1.0))                      #float
+print(type(1+2j))                     #complex
+print(type('shahnawaz'))              #string
+print(type([1,2,3,4,5,6,7,8,9,10]))   #list 
+print(type({'name'  : 'shahnawaz'}))  #dict
+print(type((1.1,1.2,1.3,1.4)))        #tuple
+print(type({1,2,3,4,5,6,7,8,9,10}))   #set
